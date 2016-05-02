1ce62c12cd1c92ea6434825813077bb3a4b284a9
edselpalmon <edsel.palmon@leisurelink.com>
2016-05-02 15:38:36 +0800

    Added spinning loading progress

---
+++ Source/HRMSWeb/Javascripts/Controllers/controller.js
@@ -0,0 +1,74 @@
+﻿// create the controller and inject Angular's $scope
+
+HRMSWeb.controller('mainController', function ($scope) {
+
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+HRMSWeb.controller('aboutController', function ($scope) {
+
+    $scope.message = 'What about page!';
+});
+
+HRMSWeb.controller('contactController', function ($scope) {
+
+    $scope.message = 'Contact Us!';
+});
+
+HRMSWeb.controller('EmployeeController', ['$scope', '$routeParams', 'mainService', function ($scope, $routeParams, mainService) {
+    //deployed URI service 
+    //$scope.URL = "http://localhost/HRMSService/EmployeeService/GetEmployeeById";
+    
+    $("#loadProgress").show();
+
+    $scope.URL = "http://localhost:55640/EmployeeService/GetEmployeeById";
+    $scope.param = $routeParams.employeeId;
+    mainService.PostData($scope.URL, $scope.param)
+        .then(function (data) {
+            $scope.Employee = data;
+            console.log(data);
+            if (data.EmployeeId > 0) {
+                $("#employeeDetail").show();
+            }
+            else {
+                $("#noDataFound").show();
+            } 
+        }, function() {
+            $("#errorMessage").show();
+        })
+        .finally(function () {
+            $("#loadProgress").hide();
+        });
+
+}]);
+
+HRMSWeb.controller('EmployeesController', ['$scope', 'mainService', function ($scope, mainService) {
+    //deployed URI service 
+    //$scope.URL = "http://localhost/HRMSService/EmployeeService/GetEmployees";
+
+    $("#loadProgress").show();
+
+    console.log($(".nav-tabs"));
+
+    $scope.URL = "http://localhost:55640/EmployeeService/GetEmployees";
+    mainService.PostData($scope.URL, $scope.param)
+        .then(function (data) {
+            $scope.Employees = data;
+            if(data != null)
+            {
+                $("#employeeList").show();
+            }
+            else
+            {
+                $("#noDataFound").show();
+            }
+        }, function() {
+            $("#errorMessage").show();
+        })
+        .finally(function () {
+            $("#loadProgress").hide();                      
+        });
+
+}]);
+
+
--- Source/HRMSWeb/Javascripts/Modules/module.js
+++ Source/HRMSWeb/Javascripts/Modules/module.js
@@ -1,2 +1,3 @@
 ﻿// create the module and name HRMSWeb
+//var HRMSWeb = angular.module('HRMSWeb', ['ngRoute', 'angular-loading-bar'])
 var HRMSWeb = angular.module('HRMSWeb', ['ngRoute'])
